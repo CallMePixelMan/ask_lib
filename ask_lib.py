7ef75c72c677d50d9ce68f3ab75719648976719d
CallMePixelMan <>
2021-08-01 17:48:10 +0200

    Ajout des docstrings

--- ask_lib.py
+++ ask_lib.py
@@ -8,7 +8,7 @@ class AskResult:
     YES = "yes"
     NO = "no"
 
-class AskFlags:
+class AskFlag:
     # Symbolic constants for aks_lib, should not be modified
     YES_TO_ALL = "yes_to_all"
     NO_TO_ALL = "no_to_all"
@@ -21,14 +21,23 @@ def ask(message, default_option = AskResult.YES, flag=None) -> bool:
         message (str): The question that will be asked to the user.
         default_option (str,optional): Must be AskResult.YES ("yes"), or AskResult.NO ("no").
             This is the default option if the user doesn't give a response / gives a unsupported response.
+        flag (Union[str,None],optional): Must be AskFlag.YES_TO_ALL ("yes_to_all"), AskFlag.NO_TO_ALL ("no_to_all"), 
+            AskFlag.DEFAULT_TO_ALL("default_to_all") or None.
+            This is a flag who can be used in a CLI when the user want to pass the confirmation.
+
 
     Returns:
         bool: True if the user accepted / False if he didn't.
             Will returns True if the user's input is unsupported, and if default_option == AskResult.YES
             Will returns False if the user's input is unsupported, and if default_option == AskResult.NO
+            Will returns True if flag == AskFlag.YES_TO_ALL
+            Will returns False if flag == AskFlag.NO_TO_ALL
+            Will returns default_option if flag == AskFlag.DEFAULT_TO_ALL
 
     Raises:
         ValueError: If the default option ins't AskResult.YES ("yes"), or AskResult.NO ("no").
+        ValueError: If the flag isn't AskFlag.YES_TO_ALL ("yes_to_all"), AskFlag.NO_TO_ALL ("no_to_all"), 
+                    AskFlag.DEFAULT_TO_ALL("default_to_all") or None.
     """
     _check_default_option(default_option)
     _check_flag(flag)
@@ -41,23 +50,35 @@ def ask(message, default_option = AskResult.YES, flag=None) -> bool:
 
     return _process_response(response, default_option)
 
+def _askresult_to_bool(askresult):
+    """Convert a AskResult constant to a boolean.
+
+    Returns:
+        bool: True if askresult == AskResult.YES ("yes"), False if if askresult == AskResult.NO ("no").
+
+    Note:
+        You should not use this function, this is an implementation detail for the ask_lib module.
+    """
+    if askresult == AskResult.YES:
+        return True
+    elif askresult == AskResult.NO:
+        return False
+
 def _check_flag(flag: str) -> None:
-    if flag not in [AskFlags.YES_TO_ALL, AskFlags.NO_TO_ALL, AskFlags.DEFAULT_TO_ALL, None]:  
+    """Check if flag is a supported value for the ask() function.
+
+    Args:
+        flag (str)
+
+    Raises:
+        ValueError: If the flag isn't AskFlag.YES_TO_ALL ("yes_to_all"), AskFlag.NO_TO_ALL ("no_to_all"), 
+                    AskFlag.DEFAULT_TO_ALL("default_to_all") or None.
+
+    Note:
+        You should not use this function, this is an implementation detail for the ask() function.
+    """
+    if flag not in [AskFlag.YES_TO_ALL, AskFlag.NO_TO_ALL, AskFlag.DEFAULT_TO_ALL, None]:  
         raise ValueError(f"'{flag}' isn't a supported flag.")
-
-def _process_flag(default_option: str, flag: str):
-    if flag == AskFlags.YES_TO_ALL:
-        return True
-    elif flag == AskFlags.NO_TO_ALL:
-        return False
-    elif flag == AskFlags.DEFAULT_TO_ALL:
-        return _askresult_to_bool(default_option)
-
-def _askresult_to_bool(default_option):
-    if default_option == AskResult.YES:
-        return True
-    elif default_option == AskResult.NO:
-        return False
 
 def _check_default_option(default_option: str) -> None:
     """Check if default_option is a supported value for the ask() function.
@@ -91,6 +112,24 @@ def _message_ending(default_option: str) -> str:
         return " [Y/n] "
     elif default_option == AskResult.NO:
         return " [y/N] "
+
+def _process_flag(default_option: str, flag: str):
+    """Figures out selected flag, and assign a boolean representation of it.
+
+    Args:
+        default_option (str): Must be "yes" or "no". Gives the function the wanted
+            default option if the user doesn't give a response / gives a unsupported response.
+        flag (str): Must be "yes_to_all", "no_to_all" or "default_to_all". Gives the function the wanted flag.
+
+    Note:
+        You should not use this function, this is an implementation detail for the ask() function.
+    """
+    if flag == AskFlag.YES_TO_ALL:
+        return True
+    elif flag == AskFlag.NO_TO_ALL:
+        return False
+    elif flag == AskFlag.DEFAULT_TO_ALL:
+        return _askresult_to_bool(default_option)
 
 def _process_response(response: str, default_option: str) -> bool:
     """Figures out user's input, and assign a boolean representation of it.
